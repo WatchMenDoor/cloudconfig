14d3234f1aaa75a80f98a7cbc650dd547af3cf15
WatchMenDoor <13218857038@163.com>
2021-03-23 20:53:59 +0800

    Initial commit

---
+++ src/main/java/com/bjsxt/configserver/ConfigServerApplication.java
@@ -0,0 +1,22 @@
+package com.bjsxt.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Auther: liuxw
+ * @Date: 2019/9/4
+ * @Description: com.bjsxt.configserver
+ * @version: 1.0
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
