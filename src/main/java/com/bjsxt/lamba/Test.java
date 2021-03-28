2ea8642ec1394739a0a25f2efcab3d7e8d6b6182
liuxinwei <13218857038@163.com>
2021-03-28 21:19:27 +0800

    增加lamba表达式简化版

--- src/main/java/com/bjsxt/lamba/Test.java
+++ src/main/java/com/bjsxt/lamba/Test.java
@@ -50,55 +50,75 @@ public class Test {
         /**
          * 无返回值，无参数
          */
-        NoReturnNoParam noReturnNoParam = () ->{
+        /*NoReturnNoParam noReturnNoParam = () ->{
             System.out.println("NoReturnNoParam");
-        };
+        };*/
+
+        //简化版
+        NoReturnNoParam noReturnNoParam = () -> System.out.println("NoReturnNoParam");
+
         noReturnNoParam.method();
         /**
          * 无返回值，有一个参数
          */
-        NoReturnOneParam noReturnOneParam =(int a)->{
+       /* NoReturnOneParam noReturnOneParam =(int a)->{
             System.out.println("noReturnOneParam"+a);
-        };
+        };*/
+        //简化版
+        NoReturnOneParam noReturnOneParam =( a)-> System.out.println("noReturnOneParam"+a);
+
         noReturnOneParam.method(10);
 
         /**
          * 无返回值，有多个参数
          */
 
-        NoReturnMultiParam noReturnMultiParam = (int a,int b)->{
+       /* NoReturnMultiParam noReturnMultiParam = (int a,int b)->{
             System.out.println("noReturnMultiParam"+a+"\t"+b);
-        };
+        };*/
+        //简化版
+        NoReturnMultiParam noReturnMultiParam = ( a,b)-> System.out.println("noReturnMultiParam"+a+"\t"+b);
+
+
         noReturnMultiParam.method(10,20);
 
         /**
          * 有返回值，无参数
          */
 
-        ReturnNoParam returnNoParam = ()->{
+       /* ReturnNoParam returnNoParam = ()->{
             System.out.print("ReturnNoParam");
             return 10;
-        };
+        };*/
+        //简化版
+        ReturnNoParam returnNoParam = () -> 10+20;
+
+
         System.out.println(returnNoParam.method());
 
         /**
          * 有返回值，有一个参数
          */
 
-        ReturnOneParam returnOneParam = (int a)->{
+       /* ReturnOneParam returnOneParam = (int a)->{
             System.out.print("returnOneParam");
             return a;
-        };
+        };*/
+        //简化版
+        ReturnOneParam returnOneParam = a->a;
         System.out.println(returnOneParam.method(10));
 
         /**
          * 有返回值，有多个参数
          */
 
-        ReturnMultiParam returnMultiParam = (int a,int b)->{
+        /*ReturnMultiParam returnMultiParam = (int a,int b)->{
             System.out.print("ReturnMultiParam");
             return a+b;
-        };
+        };*/
+        //简化版
+        ReturnMultiParam returnMultiParam = (a,b)->a+b;
+
         System.out.println(returnMultiParam.method(10,20));
     }
 }
